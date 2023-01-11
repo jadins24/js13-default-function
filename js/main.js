00e5c712595287310eb487b701ae7cc3ff9cca8f
jadins24 <jadinesh04@gmail.com>
2023-01-11 16:33:10 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,59 @@
+function myFunction(x=10,y=30){
+    let a=x+y;
+    return a;
+}
+console.log(myFunction(7,5));
+
+function myFunction1(x=5,y=10){
+    let a=x*2+y;
+    return a;
+}
+console.log(myFunction1(10,5));
+
+function myFunction2(x=12,y=24){
+    let a=x+y;
+    return a;
+}
+console.log(myFunction2(44,22));
+
+function myFunction3(x,y){
+    let a=x*y;
+    return a;
+}
+console.log(myFunction3(4,5));
+
+function myFunction4(x,y){
+    let a=x/y;
+    return a;
+}
+console.log(myFunction4(20,5));
+
+function myFunction9(x,y){
+    let a=x%y;
+    return a;
+}
+console.log(myFunction9(3,5));
+
+function myFunction5(x,y){
+    let a=x-y;
+    return a;
+}
+console.log(myFunction5(6,5));
+
+function myFunction6(x,y){
+    let a=x+y*10;
+    return a;
+}
+console.log(myFunction6(8,5));
+
+function myFunction7(x,y){
+    let a=x+y-y+x;
+    return a;
+}
+console.log(myFunction7(3,12));
+
+function myFunction8(x,y){
+    let a=x+y%10;
+    return a;
+}
+console.log(myFunction8(3,5));
